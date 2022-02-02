ffb7f9db211ba00777047f41089d3a18167f71ae
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-01 22:44:20 -0300

    code

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 let trailer = document.querySelector(".trailer");
+let video = document.querySelector(".video");
 
 function toggle() {
     trailer.classList.toggle('active');
+    video.currentTime = 0;
+    video.pause();
+    // if (trailer.classList != active) {
+    //     console.log("teste")
+    // }
 }
