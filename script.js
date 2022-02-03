bb2e8c903b492fb8c355a29bbc368e7014c73990
Leonardo Henrique de Azevedo <leonardohenriquedeazevedo@gmail.com>
2022-02-02 23:49:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let trailer = document.querySelector(".trailer");
 let video = document.querySelector(".video");
 
-// Toggle active
 function toggle() {
     trailer.classList.toggle('active');
     video.currentTime = 0;
